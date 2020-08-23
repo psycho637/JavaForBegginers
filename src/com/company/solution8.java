fb123430c714aa7df7cf2ab00a2bdea6ea0a176a
Psycho <riderenok@gmail.com>
2020-08-23 22:58:31 +0300

    8 задание

---
+++ src/com/company/solution8.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.Scanner;
+public class solution8 {
+    public static void main(String args[]) {
+        for (int i = 1; i < 51; i++) {
+            System.out.print(i + " ");
+        }
+}
+}
