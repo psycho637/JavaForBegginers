bb820294452b18bc95682bce1912f766d910b0b8
Psycho <riderenok@gmail.com>
2020-08-23 22:33:10 +0300

    5 задание

---
+++ src/com/company/solution5.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class solution5 {
+    public static void main(String args[]) {
+        Scanner str = new Scanner(System.in);
+        int num = str.nextInt();
+        System.out.println(num);
+        System.out.println(2 * num);
+        System.out.println(3 * num);
+        System.out.println(4 * num);
+        System.out.println(5 * num);
+        System.out.println(6 * num);
+        System.out.println(7 * num);
+        System.out.println(8 * num);
+        System.out.println(9 * num);
+        System.out.println(10 * num);
+    }
+}
