36f53b5949aac8bc4d61bb8bdf8483f2b2c54660
Psycho <riderenok@gmail.com>
2020-08-26 20:43:20 +0300

    7 задание попытка №3

--- src/com/company/solution7.java
+++ src/com/company/solution7.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class solution7 {
     public static void main(String args[]) {
         Scanner str = new Scanner(System.in);
-        int x = str.nextInt();
-        int y = str.nextInt();
-        int z = str.nextInt();
+        final int X = 1;
+        final int Y = 2;
+        final int Z = 3;
         int number = str.nextInt();
-        if (number == x || number == y || number == z) {
+        if (number == X || number == Y || number == Z) {
             System.out.println("Данное значение имеется в константах");
         } else  {
             System.out.println("Такой константы нет!");
