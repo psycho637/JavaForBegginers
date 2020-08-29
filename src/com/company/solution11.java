41fed42a467fdaadeb1840f1e4d20622e82f635f
Psycho <riderenok@gmail.com>
2020-08-29 21:38:11 +0300

    11 задание

---
+++ src/com/company/solution11.java
@@ -0,0 +1,8 @@
+package com.company;
+import java.util.Scanner;
+public class solution11 {
+    public static void main(String args[]) {
+        String string = new Scanner(System.in).nextLine();
+        System.out.print(string);
+    }
+}
