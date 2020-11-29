7fe6113693d169715c5bf894adbdb9d06c9e0715
Psycho <riderenok@gmail.com>
2020-11-29 15:30:38 +0300

    тест

--- src/com/company/solution4.java
+++ src/com/company/solution4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class solution4 {
     public static void main(String args[]) {
         Scanner str = new Scanner(System.in);
-        System.out.println("Введите число в бинарном формате: ");
+        System.out.println("Введите число в бинарном формате:   ");
         String bin = str.nextLine();
         int binaryNumber = Integer.parseInt(bin, 2);
         System.out.println(binaryNumber);
