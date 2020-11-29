de99dcf7e12f7ea61170072b7d99e9bcf0ac666c
Psycho <riderenok@gmail.com>
2020-11-29 15:28:33 +0300

    тест

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String args[]) {
+        char[] helloArray = {'П', 'р', 'и', 'в', 'е', 'т', '.'};
+        String helloString = new String(helloArray);
+        System.out.println(helloString);
     }
+
 }
